e3774379eda174fe563d5240adad5976495417a0
rc2pc2 <petriccarcc@gmail.com>
2023-06-15 10:05:52 +0200

    basic implementation with one-child component using vue3 and vite

---
+++ src/components/MainNavbar.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <h1>
+            La mia prima app con Vite!
+        </h1>
+        <img src="../assets/vue.svg" alt="Vue Logo">
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'MainNavbar',
+    data(){
+        return{
+
+        }
+    },
+
+    methods: {
+        getImagePath: function(img) {
+            return new URL(`../assets/${img}`, import.meta.url).href;
+        }
+    }
+}
+</script>
+
+<style lang="">
+
+</style>
